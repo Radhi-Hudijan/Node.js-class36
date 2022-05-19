2388d752d7531eab16d5217cc5f0904869d027da
Radhi Hudijan <radhif2009@gmail.com>
2022-05-19 12:36:09 +0200

    revised as per mentor request

--- hackyourtemperature/app.js
+++ hackyourtemperature/app.js
@@ -10,19 +10,22 @@ app.get("/", (req, res) => {
   res.status(200).send("<h1> Hello from backend to frontend!</h1>");
 });
 
-app.post("/weather/:city", async (req, res) => {
-  const cityName = req.params.city;
+app.post("/weather", async (req, res) => {
+  const cityName = req.body.city;
 
   try {
+    if (!cityName) {
+      res.status(400).json({ weatherText: "Please provide a city name" });
+      return;
+    }
+
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${keys.API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${keys.API_KEY}&units=metric`
     );
 
     const data = await response.json();
-    const cityTempInCelsius = Math.round(data.main.temp - 273.15);
-    res
-      .status(200)
-      .send(`The temperature of ${cityName} is : ${cityTempInCelsius} C`);
+    const cityTemp = data.main.temp;
+    res.status(200).send(`The temperature of ${cityName} is : ${cityTemp} C`);
   } catch (error) {
     res.status(404).json({ weatherText: "City is not found!" });
   }
